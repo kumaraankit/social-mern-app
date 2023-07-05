5135ab80cf98215c86300088cfa8f4dc2bce84ed
kumaraankit <ankit.k012@hotmail.com>
2023-07-05 09:59:44 +0530

    added jwt token, routes for authorization

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "./controllers/auth";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,7 +39,7 @@ const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
 
 //set up the routes for the app
-
+app.post("/routes", authRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose
